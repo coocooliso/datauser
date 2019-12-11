0148544966c04659fc5d19a34bef3dfb8b5325d2
pablo <coocooliso@gmail.com>
2019-12-11 18:18:56 +0100

    configuration

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const config = require('config');
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
@@ -5,17 +6,21 @@ const userRouter = require('./routes/userdata')
 
 require('dotenv').config();
 
-mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true} )
-.then(()=> console.log("Connected to database..."))
-.catch(err=> {
-    console.log(err);
-});
+mongoose.connect(process.env.DB_CONNECT, {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log("Connected to database..."))
+    .catch(err => {
+        console.log(err);
+    });
 
 app.use(express.json());
 
 app.use('/api/userdata', userRouter);
 
-app.listen(process.env.PORT, (err, res)=> {
-    if(err) return res.status(400).send(err);
+app.listen(config.get('DB_CONNECT'), (err, res) => {
+    if (err) return res.status(400).send(err);
     console.log(`Listening on PORT: ${process.env.PORT}`);
 });
